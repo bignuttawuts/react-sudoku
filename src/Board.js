1b0fece06a136d1f4ed7315a876db4f0f302900f
Nuttawut Singhabut <big.nuttawut.s@gmail.com>
2019-07-07 21:08:35 +0700

    implement calling API with comp life cycle

--- src/Board.js
+++ src/Board.js
@@ -26,15 +26,9 @@ const validate = board => {
 
 class Board extends Component {
   state = {
-    board: [[1, 2, 3, 4], [3, 4, 0, 0], [2, 0, 4, 0], [4, 0, 0, 2]],
-    initial: [
-      [true, true, true, true],
-      [true, true, false, false],
-      [true, false, true, false],
-      [true, false, false, true]
-    ],
     statusText: "",
-    timer: 0
+    timer: 0,
+    loading: true
   };
   componentDidMount() {
     this.interval = setInterval(() => {
@@ -42,10 +36,42 @@ class Board extends Component {
         timer: this.state.timer + 1
       });
     }, 1000);
+    this.restartBoard();
   }
   componentWillUnmount() {
     clearInterval(this.interval);
   }
+  restartBoard = () => {
+    this.setState({
+      loading: true
+    });
+    const setBoard = board => {
+      this.setState({
+        board: board,
+        timer: 0,
+        initial: board.map(row => row.map(item => item !== 0)),
+        loading: false
+      });
+    };
+    fetch(
+      "https://us-central1-skooldio-courses.cloudfunctions.net/react_01/random"
+    )
+      .then(resp => {
+        return resp.json();
+      })
+      .then(jsonResponse => {
+        setBoard(jsonResponse.board);
+      })
+      .catch(err => {
+        const defaultBoard = [
+          [1, 2, 3, 4],
+          [3, 4, 0, 0],
+          [2, 0, 4, 0],
+          [4, 0, 0, 2]
+        ];
+        setBoard(defaultBoard);
+      });
+  };
   submit = () => {
     const isValid = validate(this.state.board);
     if (isValid) {
@@ -60,23 +86,27 @@ class Board extends Component {
       <div>
         <p className="timer">Elapsed Time: {this.state.timer} seconds</p>
         <div className="board">
-          {this.state.board.map((row, i) =>
-            row.map((number, j) => (
-              <Cell
-                key={`cell-${i}-${j}`}
-                number={number}
-                isInitial={this.state.initial[i][j]}
-                onChange={newNumber => {
-                  const { board } = this.state;
-                  board[i][j] = newNumber;
-                  this.setState({
-                    board
-                  });
-                }}
-              />
-            ))
-          )}
+          {!this.state.loading &&
+            this.state.board.map((row, i) =>
+              row.map((number, j) => (
+                <Cell
+                  key={`cell-${i}-${j}`}
+                  number={number}
+                  isInitial={this.state.initial[i][j]}
+                  onChange={newNumber => {
+                    const { board } = this.state;
+                    board[i][j] = newNumber;
+                    this.setState({
+                      board
+                    });
+                  }}
+                />
+              ))
+            )}
         </div>
+        <button onClick={this.restartBoard} className="restart-button">
+          Restart
+        </button>
         <button onClick={this.submit}>Submit</button>
         <p>{this.state.statusText}</p>
       </div>
